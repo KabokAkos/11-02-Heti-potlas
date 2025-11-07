6594beb0cb5a97db39061a74c5d4445e74a05cb3
KabokAkos <99403467+KabokAkos@users.noreply.github.com>
2025-11-07 08:45:55 +0100

    Add files via upload

---
+++ Vue/calc/src/components/Calculator.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+      <h1>Calculator</h1>
+          <input type="text" v-model="screen"></input>
+      <div class="row">
+        <button @click="btnClick(b)" v-for="(n, b) in 10" class="col-3 btn btn-outline-secondary">{{ b }}</button>
+        <button @click="btnClick(b)" v-for=" b in btns" class="col-3 btn btn-outline-secondary">{{b}}</button>
+      </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let btns = ["+", "-", "/", "*", "C", "="];
+const screen = ref(0)
+const btnClick = (val) =>{
+    console.log(val)
+}
+</script>
